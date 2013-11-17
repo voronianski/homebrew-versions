81a25c8afa6dd960513640c7cf0c5be5c9347882
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-11-17 17:45:11 +0200

    update lua to version 5.2.2

--- lua52.rb
+++ lua52.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Lua52 < Formula
   homepage 'http://www.lua.org/'
-  url 'http://www.lua.org/ftp/lua-5.2.1.tar.gz'
-  sha1 '6bb1b0a39b6a5484b71a83323c690154f86b2021'
+  url 'http://www.lua.org/ftp/lua-5.2.2.tar.gz'
+  sha1 '0857e41e5579726a4cb96732e80d7aa47165eaf5'
 
   fails_with :llvm do
     build 2326
@@ -56,7 +56,7 @@ index bd9515f..5940ba9 100644
 -TO_LIB= liblua.a
 +TO_LIB= liblua.5.2.1.dylib
  TO_MAN= lua.1 luac.1
- 
+
  # Lua version and release.
 @@ -63,6 +63,8 @@ install: dummy
  	cd src && $(INSTALL_DATA) $(TO_INC) $(INSTALL_INC)
@@ -64,7 +64,7 @@ index bd9515f..5940ba9 100644
  	cd doc && $(INSTALL_DATA) $(TO_MAN) $(INSTALL_MAN)
 +	ln -s -f liblua.5.2.1.dylib $(INSTALL_LIB)/liblua.5.2.dylib
 +	ln -s -f liblua.5.2.dylib $(INSTALL_LIB)/liblua.dylib
- 
+
  uninstall:
  	cd src && cd $(INSTALL_BIN) && $(RM) $(TO_BIN)
 diff --git a/src/Makefile b/src/Makefile
@@ -72,9 +72,9 @@ index 8c9ee67..7f92407 100644
 --- a/src/Makefile
 +++ b/src/Makefile
 @@ -28,7 +28,7 @@ MYOBJS=
- 
+
  PLATS= aix ansi bsd freebsd generic linux macosx mingw posix solaris
- 
+
 -LUA_A=	liblua.a
 +LUA_A=	liblua.5.2.1.dylib
  CORE_O=	lapi.o lcode.o lctype.o ldebug.o ldo.o ldump.o lfunc.o lgc.o llex.o \
@@ -82,26 +82,26 @@ index 8c9ee67..7f92407 100644
  	ltm.o lundump.o lvm.o lzio.o
 @@ -56,11 +56,12 @@ o:	$(ALL_O)
  a:	$(ALL_A)
- 
+
  $(LUA_A): $(BASE_O)
 -	$(AR) $@ $(BASE_O)
 -	$(RANLIB) $@
 +	$(CC) -dynamiclib -install_name HOMEBREW_PREFIX/lib/liblua.5.2.dylib \
 +		-compatibility_version 5.2 -current_version 5.2.1 \
 +		-o liblua.5.2.1.dylib $^
- 
+
  $(LUA_T): $(LUA_O) $(LUA_A)
 -	$(CC) -o $@ $(LDFLAGS) $(LUA_O) $(LUA_A) $(LIBS)
 +	$(CC) -fno-common $(MYLDFLAGS) -o $@ $(LUA_O) $(LUA_A) -L. -llua.5.2.1 $(LIBS)
- 
+
  $(LUAC_T): $(LUAC_O) $(LUA_A)
  	$(CC) -o $@ $(LDFLAGS) $(LUAC_O) $(LUA_A) $(LIBS)
 @@ -106,7 +107,7 @@ linux:
  	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_LINUX" SYSLIBS="-Wl,-E -ldl -lreadline -lncurses"
- 
+
  macosx:
 -	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX" SYSLIBS="-lreadline"
 +	$(MAKE) $(ALL) SYSCFLAGS="-DLUA_USE_MACOSX -fno-common" SYSLIBS="-lreadline"
- 
+
  mingw:
  	$(MAKE) "LUA_A=lua52.dll" "LUA_T=lua.exe" \
